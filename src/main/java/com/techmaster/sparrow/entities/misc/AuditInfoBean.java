d7a700440373a5e3df4211cd5ebdd34ab34b3929
Kip <hillangat@gmail.com>
2019-01-26 22:54:38 -0500

    Few small stupid changes.

--- src/main/java/com/techmaster/sparrow/entities/misc/AuditInfoBean.java
+++ src/main/java/com/techmaster/sparrow/entities/misc/AuditInfoBean.java
@@ -3,8 +3,6 @@ package com.techmaster.sparrow.entities.misc;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.techmaster.sparrow.constants.SparrowConstants;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
